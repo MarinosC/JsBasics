efcb38b3c85931261b767cffc707ddb2b3d72d61
Marinos Christoforou <marinos@easymarkets.com>
2018-07-30 20:52:40 +0300

    4th Commit

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,15 +1,32 @@
-// Let vs Var
-var color = 'red';
-let age = 33;
+// this references:
+// method (part of an object) => object itself
+// function (not part of an object) => global (window, global)
+const video = {
+    title: 't',
+    tags: ['a', 'b', 'c'],
+    showTags() {
+        this.tags.forEach(function(tag) {
+            console.log(this.title, tag);
+        }, this );
+    }
+};
 
-// var variable is in attached to window object
-console.log(window.color); // red
+video.stop = function() {
+    console.log(this);
+};
 
-// let variable is not attached to window object
-console.log(window.age); // undefined
-
-function sayHi() {
-    console.log('hi');
+function Video(title) {
+    this.title = title;
+    console.log(this);
 }
 
-window.sayHi();
+video.stop();
+const v = new Video('Marinos');
+// Regular function by default, this, references the global object
+// If you call a function using  new operator, this, will reference a new empty object
+
+video.showTags();
+// output this.title shows undefined because it is a regular function and 
+// references the global window object.
+// To solve this problem use second parameter to foreach
+// second this references of the current object (show tags)
--- js-basics/scratchpad.js
+++ js-basics/scratchpad.js
@@ -1873,3 +1873,20 @@ function startLet() {
 }
 
 startLet();
+
+// Let vs Var
+var color = 'red';
+let age = 33;
+
+// var variable is in attached to window object
+console.log(window.color); // red
+
+// let variable is not attached to window object
+console.log(window.age); // undefined
+
+// function is attached to window object (considered bad practice)
+function sayHi() {
+    console.log('hi');
+}
+
+window.sayHi();
